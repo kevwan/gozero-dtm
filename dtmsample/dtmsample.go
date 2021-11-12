b31c9cfbcd0608b701ad7aa3ef852013846d31a3
yedongfu <dongfuye@163.com>
2021-11-12 15:46:58 +0800

    update sample

--- dtmsample/dtmsample.go
+++ dtmsample/dtmsample.go
@@ -20,9 +20,8 @@ func sagaRawCase() {
 
 func sagaCase() {
 	dtmdriverzero.RegisterAsDefault()
-	dtmurl := "etcd://127.0.0.1:2379,127.0.0.1:2380/dtmservice"       // TARGET can also be "k8s://mynamespace/dtmservice:3456"
-	transsvr := "etcd://127.0.0.1:2379,127.0.0.1:2380/trans.TransSvc" // TARGET can also be "k8s://mynamespace/trans.TransSvc:3456"
-	saga := dtmsdk.NewSagaGrpc(dtmurl, "gid2").
+	transsvr := "etcd://127.0.0.1:2379,127.0.0.1:2380/trans.TransSvc" // TODO 这个地方能够从trans的客户端便捷的获取url，而不是写死或手动拼凑
+	saga := dtmsdk.NewSagaGrpc(dtmsdk.DtmAddr, "gid2").
 		Add(transsvr+"/TransOut", transsvr+"/TransOutRevert", outReq).
 		Add(transsvr+"/TransIn", transsvr+"/TransInRevert", inReq)
 	err := saga.Submit()
--- dtmsdk/saga.go
+++ dtmsdk/saga.go
@@ -5,6 +5,9 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+// DtmAddr etcd url for DtmServer
+const DtmAddr = "etcd://127.0.0.1:2379,127.0.0.1:2380/dtmservice" // TODO 改为实际的etcd服务注册/发现的地址
+
 // SagaGrpc struct of saga
 type SagaGrpc struct {
 	dtmsdkimp.TransBase
